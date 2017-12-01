7b0553563cde1b8a94399709f096ef2b8b8bdbb0
Rich Halliday <rihallid@microsoft.com>
2017-12-01 11:49:18 -0800

    Added tests for adding individual FPRegister fields

--- neko_tests/fp_register_tests.cpp
+++ neko_tests/fp_register_tests.cpp
@@ -4,20 +4,6 @@
 
 TEST_CASE("FP Register Tests")
 {
-  SECTION("Two FP Registers can be added")
-  {
-    FPRegister reg1(1.0f, 2.11f, -3.0f, 4.0f);
-    FPRegister reg2(-2.0f, 3.05f, -4.0f, 5.0f);
-    FPRegister reg3;
-
-    addFPRegisters(&reg1, &reg2, &reg3, FP_REGISTER_ALL_FIELDS);
-
-    REQUIRE(reg3.x == -1.0f);
-    REQUIRE(reg3.y == 5.16f);
-    REQUIRE(reg3.z == -7.0f);
-    REQUIRE(reg3.w == 9.0f);
-  }
-
   SECTION("Two FP Registers can be subtracted")
   {
     FPRegister reg1(1.0f, 3.25f, -3.0f, 4.0f);
@@ -73,4 +59,61 @@ TEST_CASE("FP Register Tests")
     REQUIRE(reg3.z == 2);
     REQUIRE(reg3.w == 1);
   }
+
+  SECTION("Adding the y field leaves the other fields unchanged")
+  {
+    FPRegister reg1(3, 2, 4, 5);
+    FPRegister reg2(3, 2, 4, 5);
+    FPRegister reg3(0, 0, 2, 1);
+
+    addFPRegisters(&reg1, &reg2, &reg3, FP_REGISTER_Y_FIELD);
+
+    REQUIRE(reg3.x == 0);
+    REQUIRE(reg3.y == 4);
+    REQUIRE(reg3.z == 2);
+    REQUIRE(reg3.w == 1);
+  }
+
+  SECTION("Adding the z field leaves the other fields unchanged")
+  {
+    FPRegister reg1(3, 2, 4, 5);
+    FPRegister reg2(3, 2, 4, 5);
+    FPRegister reg3(0, 0, 2, 1);
+
+    addFPRegisters(&reg1, &reg2, &reg3, FP_REGISTER_Z_FIELD);
+
+    REQUIRE(reg3.x == 0);
+    REQUIRE(reg3.y == 0);
+    REQUIRE(reg3.z == 8);
+    REQUIRE(reg3.w == 1);
+  }
+
+  SECTION("Adding the w field leaves the other fields unchanged")
+  {
+    FPRegister reg1(3, 2, 4, 5);
+    FPRegister reg2(3, 2, 4, 5);
+    FPRegister reg3(0, 0, 2, 1);
+
+    addFPRegisters(&reg1, &reg2, &reg3, FP_REGISTER_W_FIELD);
+
+    REQUIRE(reg3.x == 0);
+    REQUIRE(reg3.y == 0);
+    REQUIRE(reg3.z == 2);
+    REQUIRE(reg3.w == 10);
+  }
+
+  SECTION("All fields in Two FP Registers can be added")
+  {
+    FPRegister reg1(1.0f, 2.11f, -3.0f, 4.0f);
+    FPRegister reg2(-2.0f, 3.05f, -4.0f, 5.0f);
+    FPRegister reg3;
+
+    addFPRegisters(&reg1, &reg2, &reg3, FP_REGISTER_ALL_FIELDS);
+
+    REQUIRE(reg3.x == -1.0f);
+    REQUIRE(reg3.y == 5.16f);
+    REQUIRE(reg3.z == -7.0f);
+    REQUIRE(reg3.w == 9.0f);
+  }
+
 }
