74f804fb446a1034cc2204f9708e7d97be219783
Rich Halliday <rihallid@microsoft.com>
2017-12-14 17:41:25 -0800

    Added empty test for clip calculations.

---
+++ neko_tests/vpu/opcode_tests/vpu_upper_clip_tests.cpp
@@ -0,0 +1,15 @@
+#include "catch.hpp"
+#include "vpu.hpp"
+#include "vpu_opcodes.hpp"
+#include "vpu_register_ids.hpp"
+#include "vpu_upper_instruction_utils.hpp"
+
+TEST_CASE("VPU Microinstruction Operation Tests")
+{
+  VPU vpu;
+  vpu.useThreads = false;
+
+  SECTION("")
+  {
+  }
+}
